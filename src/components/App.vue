534eb50167711725a3dc54351fd93e4cd31e7d08
Rene Rodriguez <rrlara@gmail.com>
2017-03-10 14:35:16 -0800

    Changes

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
     <h1>Hello: I'm offline</h1>
+
+    <el-input placeholder="Write your commit" v-model="comment" type="textarea"
+    :rows="2"></el-input>
+
+    <el-input placeholder="lat" v-model="latitude" type="number"
+    ></el-input>
+    <el-input placeholder="long" v-model="longitude" type="number"
+    ></el-input>
+
+    <el-button type="primary" size="large" style="margin: auto;">Submit</el-button>
+
+
+
     <!-- <emoji-panel v-if="source" :source="source"></emoji-panel> -->
     <!-- <loading v-else></loading> -->
   </div>
@@ -10,21 +23,28 @@
   import EmojiPanel from './EmojiPanel.vue'
   import Loading from './Loading.vue'
 
+  import {Input, Button} from 'element-ui'
+
   export default {
     name: 'app',
     data() {
       return {
-        source: null
+        comment: null,
+        latitude: null,
+        longitude: null
       }
     },
-    async created() {
-      const emojilib = await import('emojilib')
-      this.source = Object.keys(emojilib.lib).map(name => ({
-        name,
-        ...emojilib.lib[name]
-      }))
-    },
-    components: {EmojiPanel, Loading}
+    // async created() {
+    //   const emojilib = await import('emojilib')
+    //   this.source = Object.keys(emojilib.lib).map(name => ({
+    //     name,
+    //     ...emojilib.lib[name]
+    //   }))
+    // },
+    components: {
+      "el-input": Input,
+      "el-button": Button
+    }
   }
 </script>
 
